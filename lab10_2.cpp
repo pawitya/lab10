6f9b027adee4ea9ec5f0a7a25df84c5620bbc57a
pawitchaya <boss@cmu.ac.th>
2024-12-20 16:06:40 +0700

    lab10_2.cpp completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,18 +1,36 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main (){
-	ifstream source;
-	ofstream dest;
-	source.open("cheerbook.txt") ;
-	dest.open("cheerbook_copy.txt");
-	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+int main() {
+    ifstream source;
+    ofstream dest;
+
+    source.open("cheerbook.txt"); 
+    dest.open("cheerbook_copy.txt"); 
+
+    if (!source) {
+        cerr << "Error: Cannot open cheerbook.txt" << endl;
+        return 1;
+    }
+
+    if (!dest) { 
+        cerr << "Error: Cannot create cheerbook_copy.txt" << endl;
+        return 1;
+    }
+
+    dest << "-------------------- BOOM ---------------------" << endl;
+
+    string line;
+    while (getline(source, line)) {
+        dest << line << endl;
+    }
+
+    dest << "-------------------- HA!! ---------------------" << endl;
 
     source.close();
     dest.close();
-	return 0;
+
+    return 0;
 }
