b813f132d3aa5859e1b68c8b14660bdbd23b100f
pawitchaya <boss@cmu.ac.th>
2024-12-20 16:05:15 +0700

    lab10_1.cpp completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,27 +1,54 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
-	do{
-		cout << "Student [" << "]: ";
-		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
-			//Do something
-		}else if(true) // if grade is B
-			//Do something
-		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
-		} 
-	}while(true);
-	
-	
-	cout << "In total ? students.";
-	cout << "A = " << count[0] <<", ";
-	cout << "B = " << count[1] <<", ";	
-	//	and so on ... for grade = C, D, F	
-	
-	return 0;
+int main() {
+    int count[5] = {}; 
+    char grade;        
+    int totalStudents = 0; 
+
+    cout << "Please input grade of each student (A-F) or input 0 to exit.\n";
+
+    do {
+        cout << "Student [" << totalStudents + 1 << "]: ";
+        cin >> grade;
+
+        if (grade == '0') { 
+            break;
+        }
+
+        switch (grade) {
+            case 'A':
+                count[0]++;
+                totalStudents++;
+                break;
+            case 'B':
+                count[1]++;
+                totalStudents++;
+                break;
+            case 'C':
+                count[2]++;
+                totalStudents++;
+                break;
+            case 'D':
+                count[3]++;
+                totalStudents++;
+                break;
+            case 'F':
+                count[4]++;
+                totalStudents++;
+                break;
+            default:
+                cout << "Wrong input. Please input again.\n";
+                break; 
+        }
+    } while (true);
+
+    cout << "In total " << totalStudents << " students.\n";
+    cout << "A = " << count[0] << ", ";
+    cout << "B = " << count[1] << ", ";
+    cout << "C = " << count[2] << ", ";
+    cout << "D = " << count[3] << ", ";
+    cout << "F = " << count[4] << "\n";
+
+    return 0;
 }
